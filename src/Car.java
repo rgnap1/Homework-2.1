91c5364252306828e32193c19d43bd833a642371
rgnap <osovskaya_1993@mail.ru>
2023-01-12 19:10:37 +0300

    finished hw

---
+++ src/Car.java
@@ -0,0 +1,63 @@
+import java.util.Objects;
+
+public class Car {
+    String brand;
+    String model;
+    double engineVolume;
+    String color;
+    int year;
+    String country;
+
+    Car(String brand, String model, double engineVolume, String color, int year, String country){
+        this.brand = (brand == null || brand == "" ? "default" : brand);
+        this.model = (model == null || model == "" ? "default" : model);
+        this.engineVolume = (engineVolume < 0 || engineVolume == 0 ? 1.5 : engineVolume);
+        this.color = (color == null || color == "" ? "белый" : color);
+        this.year = (year < 0 || year == 0 ? 2000 : year);
+        this.country = (country == null || country == "" ? "default" : country);
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+    public String getModel() {
+        return model;
+    }
+    public double getEngineVolume() {
+        return engineVolume;
+    }
+    public String getColor() {
+        return color;
+    }
+    public int getYear() {
+        return year;
+    }
+    public String getCountry() {
+        return country;
+    }
+
+    @Override
+    public String toString() {
+        return "Car{" +
+                "brand='" + brand + '\'' +
+                ", model='" + model + '\'' +
+                ", engineVolume=" + engineVolume +
+                ", color='" + color + '\'' +
+                ", year=" + year +
+                ", country='" + country + '\'' +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Car)) return false;
+        Car car = (Car) o;
+        return Double.compare(car.getEngineVolume(), getEngineVolume()) == 0 && getYear() == car.getYear() && getBrand().equals(car.getBrand()) && getModel().equals(car.getModel()) && getColor().equals(car.getColor()) && getCountry().equals(car.getCountry());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getBrand(), getModel(), getEngineVolume(), getColor(), getYear(), getCountry());
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,17 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Car car1 = new Car("Lada","Granta",1.7,"желтый",2015,"Россия");
+        Car car2 = new Car("Audi","A8 50 L TDI quattro",3.0,"черный",2020,"Германия");
+        Car car3 = new Car("BMW","Z8",3.0,"черный",2021,"Германия");
+        Car car4 = new Car("KIA","Sportage 4",2.4,"красный",2018,"Южная Корея");
+        Car car5 = new Car("Hyundai","Avante",1.6,"оранжевый",2016,"Южная Корея");
+        System.out.println(car1.toString());
+        System.out.println(car2.toString());
+        System.out.println(car3.toString());
+        System.out.println(car4.toString());
+        System.out.println(car5.toString());
+
+
+
     }
 }
